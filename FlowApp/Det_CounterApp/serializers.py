d4f1692f338ffc1547457ad1b330e615c4b69393
Atit Kunwar <Atitkunwar2015@gmail.com>
2024-06-07 14:55:41 +0545

    admin

--- FlowApp/Det_CounterApp/serializers.py
+++ FlowApp/Det_CounterApp/serializers.py
@@ -1,23 +1,3 @@
-# import json
-# from rest_framework import serializers
-# from .models import Detection
-
-# class VehiclesSerializer(serializers.Serializer):
-#     cars = serializers.IntegerField()
-#     bikes = serializers.IntegerField()
-#     total = serializers.IntegerField()
-
-# class PositionSerializer(serializers.Serializer):
-#     value = serializers.FloatField()
-#     vehicles = VehiclesSerializer()
-
-# class DetectionSerializer(serializers.ModelSerializer):
-#     positions = serializers.DictField(child=PositionSerializer())
-
-#     class Meta:
-#         model = Detection
-#         fields = ['_id', 'location_id', 'positions']
-# serializers.py
 from rest_framework import serializers
 from .models import Detection
 
