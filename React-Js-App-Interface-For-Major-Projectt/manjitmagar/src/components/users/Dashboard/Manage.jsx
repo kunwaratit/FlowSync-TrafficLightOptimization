4593281b0a2321d012b9310711dc6d201c5fb20f
Your Name <manjitpun2058@gmail.com>
2024-06-11 23:57:11 +0545

    jsx

--- React-Js-App-Interface-For-Major-Projectt/manjitmagar/src/components/users/Dashboard/Manage.jsx
+++ React-Js-App-Interface-For-Major-Projectt/manjitmagar/src/components/users/Dashboard/Manage.jsx
@@ -1,5 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import './manage.css'; // Import your CSS file
+
 const Manage = () => {
+  const [userKey, setUserKey] = useState('');
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [selectedCity, setSelectedCity] = useState('city1'); // Set default value to 'city1'
+
+  const validateKey = () => {
+    if (userKey === 'user') {
+      setIsAuthenticated(true);
+    } else {
+      alert('Incorrect user key!');
+    }
+  };
+
+  const handleCityChange = (event) => {
+    setSelectedCity(event.target.value);
+  };
+
+  const handleDownload = () => {
+    alert(`Downloading data for ${selectedCity}`);
+  };
+
   return (
     <>
       <div className="dasheader">
@@ -7,27 +29,39 @@ const Manage = () => {
       </div>
       <hr />
       <div className="homeContainer observe">
-        <div className=" installedInfo manage-content">
-          <h3>Observe Operation</h3>
-          
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed,
-          doloribus!lorem100 Lorem ipsum dolor, sit amet consectetur adipisicing
-          elit. Debitis, est dolores, obcaecati modi sint nemo repellendus
-          voluptatibus velit provident aspernatur non! Repellendus inventore
-          accusamus ullam excepturi ipsa tempore ipsam? Magni nam nisi
-          obcaecati? Alias repellendus facilis ipsa commodi ex excepturi
-          architecto accusantium omnis delectus quisquam nesciunt nulla corrupti
-          officiis sit cumque facere iusto non quo, labore nihil maiores
-          explicabo? Veritatis sed ut sint quas, animi excepturi quia dicta
-          maxime numquam harum illo dolor ipsam repellendus ab, aspernatur qui
-          nam doloribus officia fugit incidunt ratione iste dolore officiis
-          tenetur. Ullam totam, nesciunt iusto nulla dolorem molestiae itaque ea
-          aut numquam odio.</p>
-        </div>
         <hr />
-        
+      </div>
+     <div className="center">
+      <div className="manage_cont"> 
+        {!isAuthenticated ? (
+          <div id="input-container">
+            <label htmlFor="user-key"><p>Please enter the user key:</p></label> 
+            <p>Default key 'user'</p>
+            <input
+              type="text"
+              id="user-key"
+              placeholder="Enter user key"
+              value={userKey}
+              onChange={(e) => setUserKey(e.target.value)}
+            />
+            <button onClick={validateKey}>Submit</button>
+          </div>
+        ) : (
+          
+          <div id="form-container">
+            <label htmlFor="city" className="cityy">Select Time:</label>
+            <select id="city" value={selectedCity} onChange={handleCityChange}>
+              <option value="city1">5 seconds</option> 
+              <option value="city2">10 seconds</option>
+              <option value="city3">15 seconds</option>
+            </select>
+            <button onClick={handleDownload} className="download">Download</button>
+          </div>
+        )}
+      </div>
       </div>
     </>
   );
 };
+
 export default Manage;
--- React-Js-App-Interface-For-Major-Projectt/manjitmagar/src/components/users/Dashboard/Setting.jsx
+++ React-Js-App-Interface-For-Major-Projectt/manjitmagar/src/components/users/Dashboard/Setting.jsx
@@ -1,14 +1,53 @@
-import React from "react";
-const Setting=()=>{
-    return(
-        <>
-        <div className="dasheader">
-          <h1>Setting</h1>
-        </div>
-        <hr />
+import React, { useState } from 'react';
+import './setting.css';
+import settingpic from '../../images/image.png'
 
-        
-        </>
-    )
-}
+const Setting = () => {
+  const [key, setKey] = useState('');
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const validateKey = () => {
+    if (key === 'master') {
+      setIsAuthenticated(true);
+    } else {
+      alert('Incorrect master key!');
+    }
+  };
+
+  return (
+    <>
+      <div className="dasheader">
+        <h1>Setting</h1>
+      </div>
+      <hr />
+      <div className="center">
+      <div className="setting_cont">
+        {!isAuthenticated ? (
+          <div id="input-container">
+            <label htmlFor="master-key"><p>Please enter the master key:</p></label>
+            <p>Default key 'master'</p>
+            <input
+              type="text"
+              id="master-key"
+              placeholder="Enter master key"
+              value={key}
+              onChange={(e) => setKey(e.target.value)}
+            />
+            <button onClick={validateKey}>Submit</button>
+          </div>
+        ) : (
+          <div id="content-container">
+            <img src={settingpic} alt="Description of image" />
+            <div className="buttons">
+              <button className='edit'>Edit</button>
+              <button className='save'>Save</button>
+            </div>
+          </div>
+        )}
+      </div>
+      </div>
+    </>
+  );
+};
+
 export default Setting;
