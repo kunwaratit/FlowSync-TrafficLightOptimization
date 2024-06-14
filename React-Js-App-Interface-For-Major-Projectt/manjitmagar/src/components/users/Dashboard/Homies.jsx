2ec6d6143b6585ff82d3247187b429edee0ceb1f
Atit Kunwar <Atitkunwar2015@gmail.com>
2024-06-14 06:32:38 +0545

    got

--- React-Js-App-Interface-For-Major-Projectt/manjitmagar/src/components/users/Dashboard/Homies.jsx
+++ React-Js-App-Interface-For-Major-Projectt/manjitmagar/src/components/users/Dashboard/Homies.jsx
@@ -9,64 +9,74 @@ const Homies = () => {
   console.log(search);
   return (
     <>
-      <div className="dasheader">
-        <h1>Home</h1>
-      </div>
-      <hr />
-      <div className="homeContainer">
-        <div className="search">
-          <input
-            type="search"
-            onChange={(e) => setSearch(e.target.value)}
-            name=""
-            id=""
-          />
+      {/* <div className="my-content"> */}
+        <div className="dasheader">
+          <h1>Home</h1>
         </div>
-        <div className="InstalledList">
-          {data
-            .filter((item) => {
-              return search.toLowerCase() == ""
-                ? item
-                : item.location_id.toLowerCase().includes(search);
-            })
-            .map((item) => (
-              <div className="installedInfo">
-                <div className="card" key={item.id}>
-                  <div className="roadimg">
-                    <p>{item.location_id}</p>
-                    <img src={road} alt="xaina picture nai" />
+        <hr />
+        <div className="homeContainer">
+          <div className="search">
+            <input
+              type="search"
+              onChange={(e) => setSearch(e.target.value)}
+              name=""
+              id=""
+              className="search-bar"
+              placeholder="Type location ......"
+              title="Search"
+            />
+          </div>
+          <div className="InstalledList">
+            {data
+              .filter((item) => {
+                return search.toLowerCase() == ""
+                  ? item
+                  : item.location_id.toLowerCase().includes(search);
+              })
+              .map((item) => (
+                <div className="installedInfo">
+                  <div className="card" key={item.id}>
+                    <div className="roadimg">
+                      <p>{item.location_id}</p>
+                      <span className="live">Live</span>
+                      <img src={road} alt="xaina picture nai" />
+                    </div>
+                  </div>
+                  <div className="info">
+                    <span>
+                      <strong>Note: </strong>The Junction Looks {item.situation}
+                    </span>
                   </div>
                 </div>
-                <div className="info">Looks {item.situation}</div>
-              </div>
-            ))}
+              ))}
 
-          <div className="installedInfo">
-            <div className="card">
-              <div className="roadimg">
-                <p>Gwarko</p>
-                <img src={road} alt="xaina picture nai" />
+            <div className="installedInfo">
+              <div className="card">
+                <div className="roadimg">
+                  <p>Gwarko</p>
+                  <img src={road} alt="xaina picture nai" />
+                </div>
               </div>
+              <div className="info">asbdjasdkasd</div>
             </div>
-            <div className="info">asbdjasdkasd</div>
-          </div>
-          <div className="installedInfo">
-            <div className="card">
-              <div className="roadimg">
-                <p>Ekantakuna</p>
-                <img src={road} alt="xaina picture nai" />
+            <div className="installedInfo">
+              <div className="card">
+                <div className="roadimg">
+                  <p>Ekantakuna</p>
+                  <img src={road} alt="xaina picture nai" />
+                </div>
               </div>
+              <div className="info">asbdjasdkasd</div>
             </div>
-            <div className="info">asbdjasdkasd</div>
+            <div className="installedInfo">
+              Gwarko
+              <p>asdasd</p>
+              asdasd
+            </div>
           </div>
-          <div className="installedInfo">
-            Gwarko
-            <p>asdasd</p>
-            asdasd
-          </div>
+          {/* <VehicleCountTable /> */}
         </div>
-        {/* <VehicleCountTable /> */}
-      </div>
+      {/* </div> */}
     </>
   );
 };
