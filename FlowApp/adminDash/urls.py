0e0ec7bd6236a7420ef1397cc16617602a7fba2f
Atit Kunwar <Atitkunwar2015@gmail.com>
2024-07-11 13:35:48 +0545

    asf

--- FlowApp/adminDash/urls.py
+++ FlowApp/adminDash/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
-from .views import MaskAreaAPIView, UserData,RegisterRequests
+from .views import MaskAreaAPIView, UserData,RegisterRequests,CountRequests
 from . import views
 urlpatterns = [
     path('user-data/', UserData.as_view(), name='user-data'),
     path('modify-user-data/<int:user_id>/', UserData.as_view(), name='modify-user-data'),
     path('user-reg-requests/', RegisterRequests.as_view(), name='user-reg-requests'),
     path('user-reg-requests/<int:user_id>/', RegisterRequests.as_view(), name='user-reg-requests'),
+    path('count-user/',CountRequests.as_view(),name='count-user'),
+    
     path('mask-area/', MaskAreaAPIView.as_view(), name='mask-area'),
+    
+    
     
     path('list-videos/', views.list_videos, name='list-videos'),
     path('select-video/', views.select_video, name='select-video'),
